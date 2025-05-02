1bda3fb7df53e3128095a22e3cf63636cb4aee47
chane81 <chane81@naver.com>
2025-05-02 17:33:23 +0900

    feat: initState 전달값 수정

--- src/app/context-ground/page.tsx
+++ src/app/context-ground/page.tsx
@@ -4,7 +4,7 @@ import Layout from '@/views/share/Layout';
 const contextGround = () => {
   return (
     <Layout title='Context Ground' direction='row'>
-      <Card title='Provider 1' initState={{ isOn: true }} />
+      <Card title='Provider 1' initState={{ isOn: true, count: 11 }} />
       <Card title='Provider 2' initState={{ isOn: false }} />
     </Layout>
   );
